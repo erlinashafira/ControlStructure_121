38c9f1f02aa30b036305be01b39a70776426fcaf
errerew <erlina31acc@gmail.com>
2025-10-06 15:42:33 +0700

    Membuat performa siswa berdasarkan nilai

---
+++ Prak1.py
@@ -0,0 +1,13 @@
+percentage = float(input("Masukkan persentase nilai siswa : "))
+
+if percentage >= 90:
+    print("excellant performance")
+elif percentage >= 80:
+    print("very good performance")
+elif percentage >= 70:
+    print("good performance")
+elif percentage >= 60:
+    print("averagee performance")
+else:
+    print("needs improvement")
+    
